56139b9abc24b58b3637392c766caff0c88e2acf
Jesse Houwing <jesse.houwing@gmail.com>
2026-02-17 01:40:09 +0100

    Completely reset bundling

--- packages/github-action/dist/auth/basic-auth.d.ts
+++
@@ -1,6 +0,0 @@
-import { AuthCredentials, IPlatformAdapter } from '@extension-tasks/core';
-/**
- * Get basic authentication from GitHub Actions inputs
- */
-export declare function getBasicAuth(username: string, password: string, serviceUrl: string | undefined, platform: IPlatformAdapter): Promise<AuthCredentials>;
-//# sourceMappingURL=basic-auth.d.ts.map
